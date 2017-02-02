7bfe5adf7ce57b9f1ada0caf2187b381f005c6ca
radiodario <dario.villanueva@gmail.com>
2017-02-02 13:49:14 +0000

    add hexagonal tiling

--- sketch.js
+++ sketch.js
@@ -12,6 +12,7 @@ var delta = 10;
 
 var deltaSlider;
 var angleSlider;
+var tilingTypeSelect;
 
 function setup() {
   createCanvas(400, 400);
@@ -19,7 +20,33 @@ function setup() {
   background(51);
   deltaSlider = createSlider(0, 25, 10);
   angleSlider = createSlider(0, 90, 75);
+  tilingTypeSelect = createSelect();
+  tilingTypeSelect.option('square');
+  tilingTypeSelect.option('hexagonal');
+  tilingTypeSelect.changed(chooseTiling);
 
+  chooseTiling();
+
+}
+
+function draw() {
+  background(51);
+  angle = angleSlider.value();
+  delta = deltaSlider.value();
+  for (var i = 0; i < polys.length; i++) {
+    polys[i].hankin();
+    polys[i].show();
+  }
+}
+
+function hexTiling() {
+  var hexTiles = new HexagonalTiling(50);
+  hexTiles.BuildGrid();
+  polys = hexTiles.polys;
+}
+
+function squareTiling() {
+  polys = [];
   var inc = 100;
   for (var x = 0; x < width; x += inc) {
     for (var y = 0; y < height; y += inc) {
@@ -34,13 +61,16 @@ function setup() {
   }
 }
 
-function draw() {
-  background(51);
-  angle = angleSlider.value();
-  delta = deltaSlider.value();
-  //console.log(angle, delta);
-  for (var i = 0; i < polys.length; i++) {
-    polys[i].hankin();
-    polys[i].show();
+function chooseTiling() {
+  switch (tilingTypeSelect.value()) {
+    case "square":
+      squareTiling();
+      break;
+    case "hexagonal":
+      hexTiling();
+      break;
+    default:
+      squareTiling();
+      break;
   }
 }
